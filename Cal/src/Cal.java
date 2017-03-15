9d3fab6fba08fd4f6058efd4d894577d3a7768b3
LynnLiu301 <xizi@ualberta.ca>
2017-03-15 00:53:20 -0600

    update

--- Cal/src/Cal.java
+++ Cal/src/Cal.java
@@ -3,22 +3,59 @@
  */
 
 import java.util.logging.Logger;
+import java.util.Map;
+
+
 
 public class Cal {
 
-    private String exp;
+    //private variable
+
+    private Map<String, Integer> map;
+
+    //help function 1:
+    //check if the str is a number
+    private boolean isInteger(String str){
+        try {
+            Integer.valueOf(str);
+            return true;
+        }catch (Exception e){
+            if (map.get(str) == null){
+                return false;
+            }
+            return false;
+        }
+    }
+
+    //help function 2:
+    //middleIndex
+
+
+    public static int eval(String str){
+
+        if (str.startsWith("add(")){
+            str = str.substring(4, str.length() - 1);
+            System.out.println(str);
+        }
+
+        return 0;
+    }
 
 
 
     public static void main(String[] args) {
 
+        //check if the input valid
         if (args.length != 1){
             Logger logger = Logger.getLogger("Main");
-            logger.info("There is wrong input");
+            logger.info("Invalid Input");
             return;
         }
 
         String str = args[0];
+
+        int x = eval(str);
+
 
     }
 }
